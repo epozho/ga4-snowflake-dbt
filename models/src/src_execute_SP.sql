9dc4d3f02c126c345b086bc40685a47046ecdb2c
Eric.Pozholiparambil@externals.reebok.com <Eric.Pozholiparambil@externals.reebok.com>
2023-03-30 18:02:33 +0000

    update

--- models/src/src_execute_SP.sql
+++ models/src/src_execute_SP.sql
@@ -1,8 +1,5 @@
-{{ config(
-    materialized="table",
-    database="SPARC_BASE",
-    schema="ECOM_ANALYTICS"
-)}}
+{{ config(materialized="table", database="SPARC_BASE", schema="ECOM_ANALYTICS") }}
+
 
 Select * 
 from CALL flatten_GA4_events_raw_sample();
