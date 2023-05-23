b82869b56134696464e1e3b49d06163976856ec4
Eric.Pozholiparambil@externals.reebok.com <Eric.Pozholiparambil@externals.reebok.com>
2023-05-23 17:52:59 +0000

    commit with latest deletion files

--- models/src/src_base_test.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table',database="SPARC_BASE",schema="ECOM_ANALYTICS") }}
-
-select *
-from {{ ref("src_test") }}
-limit 100
--- models/src/src_test.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized="table", schema="RBOK_GA") }}
-
-select *
-from {{ source("SPARC_RAW", "ga4_table") }}
-limit 100
